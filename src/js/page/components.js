eb51983365de227b3084f94ecf2365adc398106b
wzs28150 <1003418012@qq.com>
2019-01-04 20:53:14 +0800

    add

--- src/js/page/components.js
+++ src/js/page/components.js
@@ -1,10 +1,12 @@
- /* jshint esversion: 6 */
+/* jshint esversion: 6 */
 import debug from '../components/debug/debug';
 import Headroom from 'headroom';
 import lighter from '../components/code-lighter/code-lighter';
+import '../components/parallax/jquery.scrolly.js';
+
 // import parallax from '../components/parallax/parallax.min.js';
 // import paroller from '../components/parallax/paroller.js';
-import rallax from 'rallax.js'
+// import Parlx from 'parlx.js';
 // 右侧导航 滚动定位
 const fixed = () => {
   var myElement = document.querySelector(".page-right");
@@ -41,11 +43,15 @@ const init = (callback) => {
     style: 'light'
   };
   lighter.auto(option);
+
+  $('.parallax').scrolly({
+    bgParallax: true
+  });
+
   // 视差背景
   // $('.parallax-window').parallax({zIndex:2});
   // $('.parallax-window').paroller();
-  const parallax = rallax('.parallax-window');
-  parallax.start();
+
 };
 
 const start = {
